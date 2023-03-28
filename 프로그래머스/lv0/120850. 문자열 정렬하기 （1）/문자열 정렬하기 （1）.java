fde9a7a7a4417186ca03bdc041b1d7e3040f0202
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-03-29 00:04:52 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 9.03 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        String res = "";
+        
+        for(int i = 0; i<my_string.length(); i++){
+            if(!(my_string.substring(i,i+1).matches("^[a-zA-Z]*$")))
+                res += my_string.substring(i,i+1) + " ";
+        }
+        
+        String[] answer_s = res.split(" ");
+        
+        int [] answer_i = new int[answer_s.length];
+        
+        for(int i = 0; i<answer_i.length; i++){
+            answer_i[i] = Integer.parseInt(answer_s[i]);
+        }
+        
+        Arrays.sort(answer_i);
+        return answer_i;
+    }
+}
