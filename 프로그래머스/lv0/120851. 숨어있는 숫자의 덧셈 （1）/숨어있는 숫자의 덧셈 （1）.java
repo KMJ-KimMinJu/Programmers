59e641441241ebf903ba3b3c2500f01cdea0f7d5
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-03-30 19:17:37 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 8.38 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -4,18 +4,20 @@ class Solution {
     public int solution(String my_string) {
         String res = "";
         
+        //문자를 하나씩 자른 후 정규표현식을 사용해서 알파벳인지 아닌지 판별함
         for(int i = 0; i<my_string.length(); i++){
             if(!(my_string.substring(i,i+1).matches("^[a-zA-Z]*$")))
-                res += my_string.substring(i,i+1) + " ";
+                res += my_string.substring(i,i+1) + " ";            //알파벳이 아닐 때 하나씩 문자열에 추가
         }
         
         String[] answer_s = res.split(" ");
-        
-        int [] answer_i = new int[answer_s.length];
+ 
         int sum = 0;
-        for(int i = 0; i<answer_i.length; i++){
-            answer_i[i] = Integer.parseInt(answer_s[i]);
-            sum += answer_i[i];
+        
+        //문자로 바꿔준 후
+        for(int i = 0; i<answer_s.length; i++){
+        
+            sum += Integer.parseInt(answer_s[i]);
         }
         
         
