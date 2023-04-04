c8001a9f876fefb3d0bcbc8269ec596bd9a90962
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-04-04 19:41:18 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num, int k) {
+        
+        String str = String.valueOf(num);
+        
+        if(str.contains(String.valueOf(k)))
+            return (str.indexOf(String.valueOf(k))+1);
+        else
+            return -1;
+    }
+}
