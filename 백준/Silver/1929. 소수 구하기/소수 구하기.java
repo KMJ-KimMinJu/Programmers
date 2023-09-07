8f6812e1d1386d2143d9781b4900cef7f1da49b0
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-09-07 17:13:13 +0900

    [Silver III] Title: 소수 구하기, Time: 1044 ms, Memory: 35852 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -15,23 +15,23 @@ public class Main {
         int[] prime = new int[end+1];
 
         //소수가 아니라면 0
-        prime[0] = prime[1] = 0;
+        prime[0] = prime[1] = -1;
 
         for(int i = 2; i<=end; i++){
             prime[i]= i;
         }
 
         for(int i= 2; i<=end; i++){
-            if(prime[i]== 0){ //소수가 아닌 경우에는 패스
+            if(prime[i]== -1){ //소수가 아닌 경우에는 패스
                 continue;
             }
             for(int j = i*2; j<= end; j+= i){ //소수라면 그 수의 배수는 전부 제외
-                    prime[j] = 0;
+                    prime[j] = -1;
             }
         }
 
         for(int i = 0; i<prime.length; i++){
-            if(prime[i] != 0){
+            if(prime[i] != -1){
 
                 if(prime[i] >= start && prime[i] <= end) { //입력받은 수의 범위에 해당하는 소수만 출력
                     System.out.println(prime[i]);
