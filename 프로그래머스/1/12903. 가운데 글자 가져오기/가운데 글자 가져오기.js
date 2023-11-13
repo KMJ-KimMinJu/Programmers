0cbac6f1c6e9b6477e09ebe2d33b43699b3766d3
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-11-13 19:41:04 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,11 +1,6 @@
 function solution(s) {
     
-    
-        if(s.length === 1){
-            return s;
-        }else if(s.length === 2){
-            return s;
-        }else if(s.length % 2 === 1){
+        if(s.length % 2 === 1){
             return s.substr(s.length/2,1);
         }else{
             return s.substr(s.length/2-1,2);
