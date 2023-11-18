dd7def3eb1f07b467d460d3712aa9b4c8228b210
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-11-18 17:10:29 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,10 @@
+function solution(price, money, count) {
+    
+    let cost = 0;
+    
+    for(let i = 1; i<= count; i++){
+        cost += (price * i);
+    }
+    
+    return (cost <= money)?0 : cost-money;
+}
