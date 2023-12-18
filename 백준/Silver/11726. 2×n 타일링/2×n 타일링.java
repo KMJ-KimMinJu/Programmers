756e3875449095d135ed90d9f1091dfa7a3cedca
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2023-12-18 15:53:30 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        long[] DP = new long[N+1];
+
+        DP[1] = 1;
+
+        if(N>=2) {
+            DP[2] = 2;
+        }
+
+        for(int i = 3; i<= N; i++){
+            DP[i] = (DP[i-1]+DP[i-2])%10007;
+        }
+
+        System.out.println(DP[N]);
+    }
+}
