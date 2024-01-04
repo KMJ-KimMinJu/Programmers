4d216a73bf69dd84db95c3bee3271f132f26ccb1
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2024-01-04 21:43:32 +0900

    [level 1] Title: 대충 만든 자판, Time: 16.36 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/README.md
+++ 프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
 
 ### 성능 요약
 
-메모리: 38.2 MB, 시간: 13.71 ms
+메모리: 38.1 MB, 시간: 16.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 16:51:53
+2024년 1월 4일 21:43:28
 
 ### 문제 설명
 
--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -6,16 +6,16 @@ function solution(keymap, targets) {
         
         let count = 0;
         
-        outloop: for(let key of target.split("")){
+        for(let key of target.split("")){
             let min = 1000000000;
             
             for(let board of keymap){
                 if(board.includes(key)){
-                    min = Math.min(min, board.indexOf(key)+1);
+                    min = Math.min(min, board.indexOf(key)+1); //keymap 중에서 가장 작은 번호의 자판만 가져옴
                 }
             }
             
-            if(min === 1000000000){
+            if(min === 1000000000){ //min값이 변하지 않았다는 건 keymap에 자판이 존재하지 않음
                 count = -1;
                 break;
             }
