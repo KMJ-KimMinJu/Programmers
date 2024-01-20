c0453cb55012dd2951eba725b980e0989304f9f7
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2024-01-20 18:41:28 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 428 ms, Memory: 60180 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+
+        int N = Integer.parseInt(st.nextToken());
+
+        System.out.println(new BigInteger("2").pow(N).subtract(BigInteger.ONE));
+        hanoi(N, 1, 2, 3);
+        System.out.println(sb);
+    }
+
+    public static void hanoi(int n , int start, int mid, int end){
+
+        if(n == 0){
+            return;
+        }
+
+        hanoi(n-1, start, end, mid);
+
+        sb.append(start + " " + end+"\n");
+
+        hanoi(n-1, mid, start, end);
+    }
+}
