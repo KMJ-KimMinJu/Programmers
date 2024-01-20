00db718815171e3906ea057b11ab37e9bb256bef
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2024-01-20 17:52:21 +0900

    [level 2] Title: 하노이의 탑, Time: 6.02 ms, Memory: 99.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    
+    public static List<int[]> list = new ArrayList<>();
+    
+    public void hanoi(int n, int start, int p, int from){
+         
+        if(n == 0){
+            return;
+        }
+        
+            hanoi(n-1, start, from, p);
+            list.add(new int[] {start, from});
+            //System.out.println(n+"번 원반을 " + A + "에서" + C+"로 이동");
+            hanoi(n-1, p, start, from);
+    }
+    
+    
+    public int[][] solution(int n) {
+        hanoi(n, 1, 2, 3);
+        
+        int[][] answer = new int[list.size()][2];
+        
+
+        int index = 0;
+        for(int[] arr : list){
+            answer[index][0] = arr[0];
+            answer[index][1] = arr[1];
+            index++;
+        }
+        
+        
+        return answer;
+    }
+}
