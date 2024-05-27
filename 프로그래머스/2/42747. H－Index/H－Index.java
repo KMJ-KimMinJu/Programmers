f476fccb594c095977302bbd1b8236ba95f8c449
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2024-05-27 21:34:16 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -6,6 +6,8 @@ class Solution {
         Arrays.sort(citations); 
         
         for (int i = 0; i < citations.length; i++) {
+            
+            //인덱스만큼 지났다면 이미 i번 이상 인용된 논문이 있다는 것
             int h = citations.length - i;
             
             if (citations[i] >= h) {
