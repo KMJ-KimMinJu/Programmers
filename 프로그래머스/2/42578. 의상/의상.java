159d610677166786ae8b40b91ffe256875cfbe5d
KimMinJu <94983600+KMJ-KimMinJu@users.noreply.github.com>
2026-02-13 17:34:43 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,27 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         
-       int answer = 1;
+        HashMap<String, Integer> fashion = new HashMap<>();
         
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<clothes.length; i++){
-            String key = clothes[i][1];
+        for(int i = 0; i < clothes.length; i++){
+            String[] temp = clothes[i];
             
-            //의상의 종류를 가지고 map에 추가
-            if(!map.containsKey(key)) { 
-                map.put(key, 1);
-            } else {
-                map.put(key, map.get(key) + 1);
+            if(fashion.containsKey(temp[1])){
+                fashion.put(temp[1], fashion.get(temp[1])+1);
+            }else{
+                fashion.put(temp[1], 1);
             }
+            
         }
         
-        Iterator<Integer> it = map.values().iterator();
+        int answer = 1;
         
-        while(it.hasNext()) { //경우의 수를 계산
-            answer *= it.next().intValue()+1;
+        for(Integer i : fashion.values()){
+            
+            answer *= i+1; 
         }
         
         return answer-1;
+        
     }
 }
