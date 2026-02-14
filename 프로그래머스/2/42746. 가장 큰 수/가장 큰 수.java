d6372c089e089910b0e10e8fbabed43cb9be7ac1
KimMinJu <94983600+KMJ-KimMinJu@users.noreply.github.com>
2026-02-14 13:11:17 +0900

    [level 2] Title: 가장 큰 수, Time: 197.77 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,34 +4,25 @@ class Solution {
 
     public String solution(int[] numbers) {
         
-        String[] num_str = new String[numbers.length];
+        String[] number = new String[numbers.length];
         
-        //숫자를 string으로 변환
-        for(int i=0; i<numbers.length; i++){
-            num_str[i] = String.valueOf(numbers[i]);
+        for(int i = 0; i < numbers.length; i++){ // 숫자를 문자열로 변환
+            number[i] = String.valueOf(numbers[i]);
         }
         
-        //문자열로 따졌을 때 "610"이 큰지 "106"이 큰지 확인 후에 더 큰 걸로 정렬
-        Arrays.sort(num_str, new Comparator<String>(){
+        Arrays.sort(number, new Comparator<String>(){
             
             @Override
-            public int compare(String o1, String o2){
-      
-                String sum = o1 + o2;
-                String reverseSum = o2 + o1;
+            public int compare(String a, String b){
+                String sum1 = a+b;
+                String sum2 = b+a;
                 
-                return reverseSum.compareTo(sum);
+                return sum2.compareTo(sum1);
             }
-        });
+        });     
         
-        if(num_str[0].equals("0")) return "0";
+        String answer = (number[0].equals("0")) ? "0" : String.join("", number);
         
-        StringBuffer sb = new StringBuffer();
-        
-        for(int i = 0; i<num_str.length; i++){
-            sb.append(num_str[i]);
-        }
-        
-        return sb.toString();
+        return answer;
     }
 }
